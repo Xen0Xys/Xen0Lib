fde6d684a28ac3c24fdae79cc9dc30b2ffb054c4
Xen0Xys <red57101@gmail.com>
2021-12-05 15:03:25 +0100

    Fix Xen0Lib 1.1.3

--- src/main/java/fr/xen0xys/xen0lib/plugin/commands/XDevCommand.java
+++ src/main/java/fr/xen0xys/xen0lib/plugin/commands/XDevCommand.java
@@ -1,12 +1,14 @@
 package fr.xen0xys.xen0lib.plugin.commands;
 
-import fr.xen0xys.xen0lib.bungeecord.BungeeChannel;
 import fr.xen0xys.xen0lib.bungeecord.PluginMessage;
 import fr.xen0xys.xen0lib.plugin.Xen0Lib;
+import net.wesjd.anvilgui.AnvilGUI;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class XDevCommand implements CommandExecutor, PluginMessage {
     @Override
@@ -30,11 +32,24 @@ public class XDevCommand implements CommandExecutor, PluginMessage {
             });
 
             builder.open(player);
-             */
+
 
             player.sendMessage("Teleporting...");
             BungeeChannel channel = new BungeeChannel(Xen0Lib.getInstance(), this);
             channel.connect(player, "netherrp");
+             */
+            AnvilGUI.Builder builder = new AnvilGUI.Builder().plugin(Xen0Lib.getInstance()).itemLeft(new ItemStack(Material.PAPER)).title("Test").text("password");
+
+            builder.onComplete((_player, text) -> {
+                if(text.equalsIgnoreCase("test")) {
+                    _player.sendMessage("You have magical powers!");
+                    return AnvilGUI.Response.close();
+                } else {
+                    return AnvilGUI.Response.text("Incorrect.");
+                }
+            });
+
+            builder.open(player);
         }
         return false;
     }
